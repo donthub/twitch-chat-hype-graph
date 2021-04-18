f220b904c1fe71fef984f60a423b469a5f1742ae
Beni B <dontsmail@gmail.com>
2021-04-18 18:17:26 +0200

    Added message for ID input.

--- util.py
+++ util.py
@@ -6,6 +6,6 @@ from easygui import enterbox
 class Util:
 
     def get_id(self):
-        url = enterbox()
+        url = enterbox("Enter Twitch VOD URL or ID:")
         pattern = re.compile(r'^.*?(\d+).*?$')
         return pattern.match(url).group(1)
